a63385421b15ecaa9ac934e7095789782c760a70
obiwan76 <marcio.melendez@gmail.com>
2025-03-18 13:05:39 -0400

    added TODO note about change for later Python release

--- stpsf/utils.py
+++ stpsf/utils.py
@@ -206,6 +206,8 @@ def auto_download_stpsf_data():
             # Extract the tarball
             with tarfile.open(filename, "r:gz") as tar:
                 tar.extractall(default_path.parent)
+	# TODO filter="fully_trusted" will need to be added as a parameter once python 3.14
+	# is adopted due to advertised changing defaults in python.
 
         if not any(default_path.iterdir()):
             raise IOError(f"Failed to get and extract STPSF data files to {default_path}")
